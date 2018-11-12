a12d36596a29d66d6e9765e528157be836dd0b2c
wkr1u18 <wkr1u18@soton.ac.uk>
2018-11-12 13:34:34 +0000

    CyclicFixed two

---
+++ CyclicFixed.java
@@ -0,0 +1,39 @@
+/**
+ * Class for modelling cyclic fixed appliances in the simulation. It inherits from {@link Appliance} class.
+ * @author Wojciech Rozowski (wkr1u18)
+ *
+ */
+
+public class CyclicFixed extends Appliance{
+	private float unitsUsage;
+	private int activeHours; 
+	
+	private int internalClock = 0;
+	
+	/**
+	 * CyclicFixed constructor taking initialising Appliance master class fields as well as class-specific fields. ActiveHours must be in range [1,24]
+	 * @param name String containing appliance name.
+	 * @param meter Reference for {@link Meter} object managing this appliance usage.
+	 * @param unitsUsage float describing unit's usage of this device
+	 * @param activeHours int describing number of hours, when the device is active.
+	 * @throws IllegalArgumentException when activeHours is not in range [1, 24]
+	 */
+	public CyclicFixed(String name, Meter meter, float unitsUsage, int activeHours) throws IllegalArgumentException {
+		super(name, meter);
+		this.unitsUsage = unitsUsage;
+		this.activeHours = activeHours;
+		if(this.activeHours < 1 || this.activeHours > 24) {
+			throw new IllegalArgumentException("activeHours out of range: " + this.activeHours + " expected range: [1,24]");
+		}
+	}
+	@Override
+	public void timePasses() {
+		if(internalClock == 24) {
+			internalClock = 0;
+		}
+		if(internalClock<activeHours) {
+			internalClock++;
+			//consume
+		}
+	}
+}
